22b9c2540492a9b0e17cccca4402e48d00025e3d
Benjamin Leetmaa <benjaminleetmaa@gmail.com>
2025-07-16 16:18:16 +0200

    WIP

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true
+      },
+      protocolImports: true
+    })
+  ],
   build: {
     target: 'es2015',
     minify: false
